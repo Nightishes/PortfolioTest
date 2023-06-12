65509a687a6856e635187aac30c9a3731cfb5508
Nightishes <q.bathany@gmail.com>
2023-06-12 15:48:13 +0200

    Update Footer.jsx

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
             <h2>Me contacter :</h2>
             <a href='mailto:q.bathany@gmail.com'><img id='email-icon' alt='My email' src={Email}></img></a>
             <a href='https://github.com/Nightishes' target='_blank' rel="noreferrer noopener"><img id='github-icon' alt='My Github' src={Github}></img></a>
-            <img id='linkedin-icon' alt='My Linkedin' src={Linkedin}></img>
+            <a href='https://www.linkedin.com/in/quentin-bathany-a53a9727a/' target='_blank' rel="noreferrer noopener"><img id='linkedin-icon' alt='My Linkedin' src={Linkedin}></img></a>
         </section>
         </footer>
         </>
